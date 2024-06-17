666aa0e34b8a546a0a2da5d462be3a737679bb7c
andreyalf1961 <bazulin.a.i@gmail.com>
2024-06-17 21:29:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#Задача "Длина слова"
+a=("hello")
+print(len(a))
+
+#Задача "Суммы и разности"
+first=5
+second=3
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+
+#Задача "Среднее арифметическое"
+first=5
+second=6
+third=7
+mean=(first+second+third)/3
+print(mean)
+
+#Задача "Простые строчки"
+first_string="вторник"
+second_string="понедельник"
+print(second_string+", "+first_string)
+
+#Задача "Сложная  формула"
+a=2
+b=3
+c=4
+result_1=(a*b)+(a*c)
+result_2=result_1**3/2
+print(result_2)
+
+
+
